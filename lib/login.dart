517a82233b3e0e2370c22dd96bf2170f9fa2f62b
Pulindu <wppulindum@gmail.com>
2025-02-27 22:38:18 +0530

    Update all project files including assets and lib folder

---
+++ lib/login.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'signup.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.white, Colors.lightBlueAccent, Colors.blue],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(90),
+                    bottomRight: Radius.circular(90),
+                  ),
+                ),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/Logoostocare.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "OstoCare",
+                      style: TextStyle(fontSize: 35, fontWeight: FontWeight.w200, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Log in ",
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent),
+                    ),
+                    Text(
+                      "to your account",
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 30),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        hintText: "Enter your email",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      obscureText: _isObscure,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter your password",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                        suffixIcon: IconButton(
+                          icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          // Add forget password navigation logic here
+                        },
+                        child: Text("Forgot Password?"),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add authentication logic here
+                      },
+                      style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+                      child: Text("Login", style: TextStyle(color: Colors.white)),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                      },
+                      child: Text("Don't have an account? Sign Up"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 40,
+            left: 16,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black, size: 28),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/signup.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'login.dart';  // To navigate to the login page if the user already has an account
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.white, Colors.lightBlueAccent, Colors.blue],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(90),
+                    bottomRight: Radius.circular(90),
+                  ),
+                ),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/Logoostocare.png', width: 150, height: 150),
+                    SizedBox(height: 20),
+                    Text("OstoCare", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w200, color: Colors.white)),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Create ", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent)),
+                          Text("your account", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                      SizedBox(height: 70),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                          hintText: "Enter your Username",
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          hintText: "Enter your Email",
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        obscureText: _obscurePassword,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "Enter your Password",
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                          suffixIcon: IconButton(
+                            icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                _obscurePassword = !_obscurePassword;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        obscureText: _obscureConfirmPassword,
+                        decoration: InputDecoration(
+                          labelText: "Confirm Password",
+                          hintText: "Confirm your Password",
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                          suffixIcon: IconButton(
+                            icon: Icon(_obscureConfirmPassword ? Icons.visibility_off : Icons.visibility),
+                            onPressed: () {
+                              setState(() {
+                                _obscureConfirmPassword = !_obscureConfirmPassword;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Center(
+                        child: SizedBox(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+                            child: Center(
+                              child: Text("Create Account", style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 20,
+            left: 17,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black, size: 28),
+              onPressed: () {
+                Navigator.pop(context);  // Navigate back to LoginPage
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
